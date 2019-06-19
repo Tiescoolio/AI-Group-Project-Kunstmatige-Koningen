734f006e54501e42e86426c61be8691cf15325d6
Nick Roumimper <nick.roumimper@hu.nl>
2019-06-19 13:54:50 +0200

    Added beta functions in order to get the webshop project linked to the desired billing account.

--- helloworld/autodeploy.sh
+++ helloworld/autodeploy.sh
@@ -7,6 +7,13 @@ HUPROJECTNAME="huwebshop-$HUPROJECTNAME"
 # echo $HUPROJECTNAME
 gcloud projects create $HUPROJECTNAME --set-as-default
 gcloud app create --project=$HUPROJECTNAME --region=europe-west
+echo "In order to deploy this application, you have to link a billing account."
+echo "Note that the gcloud methods used here are in beta, and may fail over time."
+echo "The following billing accounts are associated with this user:"
+gcloud beta billing account list
+echo "Please input the full ACCOUNT_ID of the billing account you want to use:"
+read BILLINGACCOUNT
+gcloud beta billing projects link $HUPROJECTNAME --billing-account $BILLINGACCOUNT
 gcloud services enable cloudbuild.googleapis.com
 gcloud app deploy
 
