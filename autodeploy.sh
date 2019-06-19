4b92e3ef376ac52a66522139732bd7ab6a7cd592
Nick Roumimper <nick.roumimper@hu.nl>
2019-06-19 12:39:19 +0200

    Adding the first version of the autodeploy shell script, which would allow us to instantly deploy this project from the gcloud shell.

---
+++ autodeploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+HUPROJECTNAME=`date +"%Y%m%d%H%M%S"`
+HUPROJECTNAME="huwebshop-$HUPROJECTNAME"
+echo $HUPROJECTNAME
+# git clone https://github.com/nickroumimper/huwebshop.git
+
+# gcloud projects create [YOUR_PROJECT_NAME] --set-as-default
+
+# gcloud projects create huwebshop-`date +"%Y-%m-%d-%H-%M-%S"`
