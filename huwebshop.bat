b9769c6e505cccd0bb7f956e2622bb7dab5583b9
Nick Roumimper <nick.roumimper@hu.nl>
2019-06-20 13:17:17 +0200

    Rewrote code, merged the remote setup, which is currently dependent on the .env file whether or not to contact the cluster.

--- huwebshop.bat
+++
@@ -1,3 +0,0 @@
-rem reference file for windows running
-rem set FLASK_APP=huwebshopremote.py
-rem python -m flask run
---
+++ huwebshopsetup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Shell script for automatically running this project on a local environment."
+echo "Please respond to the prompts as they come up."
+echo "Please provide the name of the cluster you want to connect to (e.g. huwebshoptest-neick.mongodb.net):"
+read -p 'Cluster name: ' MONGODBSERVER
+echo "Please provide your username for this cluster (e.g. accessUser):"
+read -p 'Username: ' MONGODBUSER
+echo "Please provide your password for this cluster:"
+read -sp 'Password: ' MONGODBPASSWORD
+rm -f .env
+echo "MONGODBSERVER=$MONGODBSERVER" >> .env
+echo "MONGODBUSER=$MONGODBUSER" >> .env
+echo "MONGODBPASSWORD=$MONGODBPASSWORD" >> .env
+export FLASK_APP=huwebshop.py
+python -m flask run
