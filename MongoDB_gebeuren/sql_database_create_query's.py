a81d3d39641cd36209ff23d5dc0616788f14f4b4
Ties <ties.smid@student.hu.nl>
2024-03-25 14:37:02 +0100

    kleine verandering in id int gemaakt

--- MongoDB_gebeuren/sql_database_create_query's.py
+++ MongoDB_gebeuren/sql_database_create_query's.py
@@ -1,6 +1,6 @@
 import psycopg2 as ps
 
-hostname = "20.108.157.88"
+hostname = "localhost"
 database = "AI Group Project"
 username = "postgres"
 pwd = ""
@@ -16,7 +16,7 @@ con = ps.connect(
 cur = con.cursor()
 
 products_tabel = """CREATE TABLE IF NOT EXISTS products (
-                id  NOT NULL PRIMARY KEY,
+                id INT NOT NULL PRIMARY KEY,
                 name VARCHAR(255),
                 brand VARCHAR(100), 
                 category VARCHAR(100),
@@ -25,29 +25,32 @@ products_tabel = """CREATE TABLE IF NOT EXISTS products (
                 sub_sub_sub_category VARCHAR(100),
                 gender VARCHAR(50),
                 target_audience VARCHAR(100),
-                selling_price  int(10),
-                mrsp  int(10),
-                price_discount    int(10),
-                availability  int(10),
+                selling_price  INT,
+                mrsp  INT,
+                price_discount    INT,
+                availability  INT,
                 aanbiedingen  VARCHAR(255),
                 recommendable BOOLEAN
                 )"""
 
-profiles_tabel = """CREATE TABLE IF NOT EXISTS profiles (
-                    id  NOT NULL PRIMARY KEY,
-                    viewed_before_id FOREIGN KEY(viewed_before_id),
-                    similars_id  FOREIGN KEY(similars_id),
-                    ordered_id  FOREIGN KEY(ordered_id)
-                    )"""
-
 
 viewed_before_tabel = """CREATE TABLE IF NOT EXISTS viewed_before (
-                    id  NOT NULL PRIMARY KEY
+                    id INT NOT NULL PRIMARY KEY
                     )"""
 
 similars_tabel = """CREATE TABLE IF NOT EXISTS similars(
-                    id  NOT NULL PRIMARY KEY
+                    id INT NOT NULL PRIMARY KEY
                     )"""
 
 ordered_tabel = """CREATE TABLE IF NOT EXISTS ordered (
-                    id  NOT NULL PRIMARY KEY)"""
+                    id INT NOT NULL PRIMARY KEY)"""
+
+profiles_tabel = """CREATE TABLE IF NOT EXISTS profiles (
+                    id INT NOT NULL PRIMARY KEY,
+                    viewed_before_id INT,
+                    FOREIGN KEY(viewed_before_id) REFERENCES viewed_before(id),
+                    similars_id INT, 
+                    FOREIGN KEY(similars_id) REFERENCES similars(id),
+                    ordered_id INT,
+                    FOREIGN KEY(ordered_id) REFERENCES ordered(id)
+                    )"""
