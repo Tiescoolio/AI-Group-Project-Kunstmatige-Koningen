bd602809ac1bd509b4d7c826e20836a8840a0ac4
Ties <ties.smid@student.hu.nl>
2024-03-25 10:17:34 +0100

    data overzetten van products

---
+++ MongoDB_gebeuren/data_overzetten.py
@@ -0,0 +1,25 @@
+from IPython.display import display
+from pymongo import MongoClient
+import pandas
+
+pandas.set_option('display.max_rows', None)
+pandas.set_option('display.max_columns', None)
+pandas.set_option('display.width', None)
+
+def connect_to_mongo(host, port, db):
+    conn = MongoClient(host,port)
+    return conn[db]
+
+def turn_mongo_to_sql():
+    data = get_mongo()
+    display(data)
+
+def get_mongo():
+    collectie = "products"
+    database = connect_to_mongo("localhost", 27017, "huwebshop")
+    cursor = database[collectie].find({}, {"_id": 1, "name": 1, "category": 1, "brand": 1})
+    print(cursor)
+    data = pandas.DataFrame(list(cursor))
+    return data
+
+turn_mongo_to_sql()
