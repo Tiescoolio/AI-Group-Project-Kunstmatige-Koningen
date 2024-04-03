a31c65a8cf0479d4d41111f6835245fa74639454
Ties <ties.smid@student.hu.nl>
2024-04-03 15:50:20 +0200

    zonder sub sub sub category en availability aangezien daar gezeik mee is, dus fixen we later

--- MongoDB_gebeuren/create_queries/products_query.sql
+++ MongoDB_gebeuren/create_queries/products_query.sql
@@ -5,13 +5,11 @@ CREATE TABLE IF NOT EXISTS products (
     category VARCHAR(100),
     sub_category VARCHAR(100),
     sub_sub_category VARCHAR(100),
-    sub_sub_sub_category VARCHAR(100),
     gender VARCHAR(50),
     target_audience VARCHAR(100),
     selling_price  INT,
     mrsp  INT,
     price_discount    INT,
-    availability  INT,
     aanbiedingen  VARCHAR(255),
     recommendable BOOLEAN
 )
