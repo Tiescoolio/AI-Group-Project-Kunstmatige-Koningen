57ea883fc69d7ee8a65972a77be0cfd5965b3cb3
Ties <ties.smid@student.hu.nl>
2024-04-03 15:45:10 +0200

    comments toegevoegd dat uitlegt op welke volgorde de data eruit komt

--- MongoDB_gebeuren/data_overzetten.py
+++ MongoDB_gebeuren/data_overzetten.py
@@ -1,6 +1,6 @@
 import pandas
 from pymongo import MongoClient
-
+import time
 
 
 pandas.set_option('display.max_rows', None)
@@ -18,8 +18,9 @@ def get_mongo():
     collectie = "products"
     database = connect_to_mongo("localhost", 27017, "huwebshop")
     cursor = database[collectie].find({}, {"_id":1, "name":1, "category":1, "brand":1, "gender":1, "sub_category":1, "sub_sub_category":1, "sub_sub_sub_category":1, "properties.doelgroep":1, "price.selling_price":1,"price.mrsp":1, "price.price_discount":1, "properties.availability":1, "properties.discount":1, "recommendable":1})
-
-    #alfabetische volgorde
+#alfabetische volgorde: id, brand,category, gender, name, mrsp, price_discount, selling_price, availability, discount, doelgroep, recommendable, sub_category, sub_sub_category, sub_sub_sub_category
+#realistische volgorde (omdat waarom de fuck zou code niet gewoon geheel op alfabetische volgorde werken als het er staat weetje:
+#id, brand, category, gender, name, mrsp, selling_price, price_discount, discount, doelgroep, recommendable, sub_category, sub_sub_category
     data = pandas.DataFrame(list(cursor))
     data = data.where(pandas.notnull(data), None)
 
