230998f6afc4e071df3417f75ac63da8082329e5
Tomkr <tomkruijk@gmail.com>
2024-04-03 16:46:51 +0200

    Data_overzetten bijgewerkt op de availablity

--- MongoDB_gebeuren/data_overzetten.py
+++ MongoDB_gebeuren/data_overzetten.py
@@ -11,6 +11,24 @@ def connect_to_mongo(host, port, db):
     conn = MongoClient(host,port)
     return conn[db]
 
+def get_availability():
+    collectie = "products"
+    database = connect_to_mongo("localhost", 27017, "huwebshop")
+    cursor = database[collectie].find({}, {"_id": 1, "properties.availability": 1})
+    data = pandas.DataFrame(list(cursor))
+    data = data.where(pandas.notnull(data), None)
+
+    templist = []
+    for index, row in data.iterrows():
+        test = row.values
+        key = test[0]
+        for i in row.values:
+            if isinstance(i, dict):
+                for j in i.values():
+                    if int(j) > 0:
+                        templist.append(key)
+    return templist
+
 def get_mongo():
     collectie = "products"
     database = connect_to_mongo("localhost", 27017, "huwebshop")
@@ -25,7 +43,6 @@ def get_mongo():
     templist = []
     for index, row in data.iterrows():
         indices = []
-        #print(len(row.values))
         for i in row.values[:-1]:
             if isinstance(i, dict):
                 for j in i.values():
@@ -34,7 +51,14 @@ def get_mongo():
                 indices.append(i)
         if len(indices) == 13:
             templist.append(indices)
-    return templist
+    products = []
+    opties = get_availability()
+    for i in templist:
+        for j in opties:
+            if i[0] == j:
+                products.append(i)
+    return products
 
+get_mongo()
 if __name__ == '__main__':
     get_mongo()
--- MongoDB_gebeuren/profiles_sql_idk.py
+++ MongoDB_gebeuren/profiles_sql_idk.py
@@ -1,7 +1,6 @@
 from IPython.display import display
 from pymongo import MongoClient
 import pandas
-
 pandas.set_option('display.max_rows', None)
 pandas.set_option('display.max_columns', None)
 pandas.set_option('display.width', None)
@@ -18,11 +17,11 @@ def get_mongo():
     collectie = "profiles"
     database = connect_to_mongo("localhost", 27017, "huwebshop")
     cursor = database[collectie].find({}, {"_id":1})
-
-    #alfabetische volgorde
-    print(cursor)
     data = pandas.DataFrame(list(cursor))
-    return data
-
-turn_mongo_to_sql()
-
+    templist = []
+    for i, temp in data.iterrows():
+        templist.append(i)
+    print(templist)
+    return templist
+#turn_mongo_to_sql()
+import time
