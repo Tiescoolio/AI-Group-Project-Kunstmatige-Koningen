ef7bdf9bee4f9fc58f72768cc9c41f7f4cedc5ad
Ties <ties.smid@student.hu.nl>
2024-04-04 13:20:28 +0200

    alles in functies gezet, en gezorgt dat als we klaar zijn met de profiel data overzetten naar een goed datatype dat het gelijk zou moeten werken

--- MongoDB_gebeuren/query_statements/sql_insert_query's.py
+++ MongoDB_gebeuren/query_statements/sql_insert_query's.py
@@ -1,4 +1,5 @@
 from mongodb_data.products_data import get_mongo
+from mongodb_data.products_data import get_mongo
 import psycopg2 as ps
 producten = get_mongo()
 for product in producten:
@@ -20,9 +21,43 @@ con = ps.connect(
     port = port_id)
 cur = con.cursor()
 
-products_insert_query = """INSERT INTO products (id, name, brand, category, sub_category, sub_sub_category, gender, target_audience, selling_price, mrsp, price_discount, aanbiedingen, recommendable)
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-# for product in producten:
-#     cur.execute(products_insert_query, product)
+def products():
+    products_insert_query = """INSERT INTO products (id, name, brand, category, sub_category, sub_sub_category, gender, target_audience, selling_price, mrsp, price_discount, aanbiedingen, recommendable)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+    for product in producten:
+        cur.execute(products_insert_query, product)
+
+def profiles():
+    profiles_insert_query = """INSERT INTO profiles (id) VALUES (%s)"""
+    for profiel in profiles:
+        cur.execute(profiles_insert_query, profiel[0])
+
+def ordered():
+    ordered_insert_query = """INSERT INTO ordered (id, profile_id) VALUES (%s, %s)"""
+    ordered = []
+    for profiel in profiles:
+        for order_id in profiel[1]:
+            ordered.append(profiel[0], order_id)
+    for order in ordered:
+        cur.execute(ordered_insert_query, order)
+
+def similar():
+    similar_insert_query = """INSERT INTO similars (id, profile_id) VALUES (%s, %s)"""
+    vergelijkbaren = []
+    for profiel in profiles:
+        for vergelijkbare_id in profiel[2]:
+            ordered.append(profiel[0], vergelijkbare_id)
+    for vergelijkbare in vergelijkbaren:
+        cur.execute(similar_insert_query, vergelijkbare)
+
+def viewed_before():
+    viewed_before_insert_query = """INSERT INTO viewed_before (id, profile_id) VALUES (%s, %s)"""
+    viewed_before = []
+    for profiel in profiles:
+        for viewed_id in profiel[3]:
+            ordered.append(profiel[0], viewed_id)
+    for bekeken in viewed_before:
+        cur.execute(viewed_before_insert_query, bekeken)
+
 con.commit()
 con.close()
