9a84842739cd7bbc70fea864626fc00d1b6e9f46
Tomkr <tomkruijk@gmail.com>
2024-04-04 14:18:42 +0200

    SQL data overzetten gerelateerd

--- MongoDB_gebeuren/query_statements/mongodb_data/profiles_data.py
+++ MongoDB_gebeuren/query_statements/mongodb_data/profiles_data.py
@@ -13,16 +13,29 @@ def turn_mongo_to_sql():
     data = get_mongo()
     display(data)
 
+import time
 def get_mongo():
     collectie = "profiles"
     database = connect_to_mongo("localhost", 27017, "huwebshop")
-    cursor = database[collectie].find({}, {"_id":1})
+    cursor = database[collectie].find({}, {"_id": 1, "order.ids":1, "recommendations.viewed_before":1, "recommendations.similars":1})
     data = pandas.DataFrame(list(cursor))
+    data = data.where(pandas.notnull(data), None)
+
+    #_ID, ORDER, Recommendations
     templist = []
-    for i, temp in data.iterrows():
-        templist.append(i)
-    print(templist)
+    for index, row in data.iterrows():
+        indices = []
+        for i in row.values:
+            if i == row.values[0]:
+                indices.append(row.values[0])
+            else:
+                if isinstance(i, dict):
+                    for j in i.values():
+                        indices.append(j)
+                else:
+                    indices.append(i)
+        templist.append(indices)
     return templist
-#turn_mongo_to_sql()
+turn_mongo_to_sql()
 
-
+print("KLAAR")
