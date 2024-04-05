171bbf32305ba62a0b8d56e7dd3b2fee4173ec2f
Ties <ties.smid@student.hu.nl>
2024-04-05 14:26:31 +0200

    sessions gemaakt en sessions_products

--- MongoDB_gebeuren/query_statements/sql_insert_query's.py
+++ MongoDB_gebeuren/query_statements/sql_insert_query's.py
@@ -1,19 +1,16 @@
-import os
-from dotenv import load_dotenv
 from mongodb_data.products_data import get_mongo
-from mongodb_data.products_data import get_mongo
+from mongodb_data.profiles_data import get_mongo as profiles_data
 import psycopg2 as ps
 producten = get_mongo()
 for product in producten:
     product[0], product[1], product[2], product[3], product[4], product[5], product[6], product[7], product[8], product[9], product[10], product[11], product[12] \
         =  product[0], product[4], product[1], product[2], product[11], product[12], product[3], product[9], product[6], product[5], product[7], product[8], product[10]
-
-load_dotenv()
+profielen = profiles_data()
 
 hostname = "localhost"
-database = "hu_webshop"
+database = "AI Group Project"
 username = "postgres"
-pwd = os.getenv("db_password")
+pwd = "Gymhond11"
 port_id = 5432
 #variabelen aanmaken om makkelijker in een keer pycharm te verbinden met postgres
 con = ps.connect(
@@ -21,8 +18,7 @@ con = ps.connect(
     dbname = database,
     user = username,
     password= pwd,
-    # port = port_id
-)
+    port = port_id)
 cur = con.cursor()
 
 def products():
@@ -31,37 +27,63 @@ def products():
     for product in producten:
         cur.execute(products_insert_query, product)
 
-def profiles():
+def profiles(profiles):
     profiles_insert_query = """INSERT INTO profiles (id) VALUES (%s)"""
     for profiel in profiles:
-        cur.execute(profiles_insert_query, profiel[0])
+        profiel_id = str(profiel[0])
+        cur.execute(profiles_insert_query, (profiel_id,))
 
-def ordered():
-    ordered_insert_query = """INSERT INTO ordered (id, profile_id) VALUES (%s, %s)"""
-    ordered = []
+def sessions(profiles):
+    ordered_insert_query = """INSERT INTO sessions (buid, profile_id) VALUES (%s, %s)"""
+    sessions = []
     for profiel in profiles:
-        for order_id in profiel[1]:
-            ordered.append(profiel[0], order_id)
-    for order in ordered:
-        cur.execute(ordered_insert_query, order)
+        if profiel[1] == None:
+            continue
+        else:
+            profiel_id = str(profiel[0])
+            for buid in profiel[1]:
+                sessions.append([buid, profiel_id])
+            for session in sessions:
+                cur.execute(ordered_insert_query, session)
 
-def similar():
+def similar(profiles):
     similar_insert_query = """INSERT INTO similars (id, profile_id) VALUES (%s, %s)"""
     vergelijkbaren = []
     for profiel in profiles:
-        for vergelijkbare_id in profiel[2]:
-            ordered.append(profiel[0], vergelijkbare_id)
-    for vergelijkbare in vergelijkbaren:
-        cur.execute(similar_insert_query, vergelijkbare)
+        if profiel[2] == None:
+            continue
+        else:
+            profiel_id = str(profiel[0])
+            for vergelijkbare_id in profiel[2]:
+                vergelijkbaren.append([vergelijkbare_id, profiel_id])
+            for vergelijkbare in vergelijkbaren:
+                cur.execute(similar_insert_query, vergelijkbare)
 
-def viewed_before():
+def viewed_before(profiles):
     viewed_before_insert_query = """INSERT INTO viewed_before (id, profile_id) VALUES (%s, %s)"""
     viewed_before = []
     for profiel in profiles:
-        for viewed_id in profiel[3]:
-            ordered.append(profiel[0], viewed_id)
-    for bekeken in viewed_before:
-        cur.execute(viewed_before_insert_query, bekeken)
+        if profiel[3] == None:
+            continue
+        else:
+            profiel_id = str(profiel[0])
+            for viewed_id in profiel[3]:
+                viewed_before.append([viewed_id, profiel_id])
+            for bekeken in viewed_before:
+                cur.execute(viewed_before_insert_query, bekeken)
 
+def session_products(sessies):
+    sessions_products_insert = """INSERT INTO sessions_products (sessions_buid, id) VALUES (%s, %s)"""
+    sessions = []
+    for sessie in sessies:
+        if sessie[1] == None:
+            continue
+        else:
+            for product in sessie[1]:
+                sessions.append([sessie[0], product])
+            for session in sessions:
+                cur.execute(sessions_products_insert, session)
+
+viewed_before(profielen)
 con.commit()
 con.close()
