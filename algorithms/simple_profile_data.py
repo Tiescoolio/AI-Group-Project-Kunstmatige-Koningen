fc2efa8c682008f4e91187ccafb60c72cd48d5da
StanMerlijn <144694971+StanMerlijn@users.noreply.github.com>
2024-04-05 14:17:11 +0200

    data migration for testing simple recommendation

--- algorithms/simple_profile_data.py
+++ algorithms/simple_profile_data.py
@@ -3,36 +3,36 @@ import pymongo
 import time
 
 
-def insert_prod_data(data, cur) -> None:
+def insert_prof_data(data, cur) -> None:
     # Truncate existing data to avoid duplicate data
-    cur.execute("""TRUNCATE TABLE public.products""")
+    cur.execute("""TRUNCATE TABLE public.profiles""")
 
     sql_query = """
     INSERT INTO products (id, brand, category, sub_category, sub_sub_category, aanbiedingen, recommendable)
     VALUES (%s, %s, %s, %s, %s, %s, %s);
     """
-    for num, prod in enumerate(data):
-        keys = prod.keys()
+    for num, prof in enumerate(data):
+        keys = prof.keys()
 
         # Set discount to None to insert correct NULL value
         # Checks for some weird products.
-        properties = prod.get("properties", {})
+        properties = prof.get("properties", {})
         if properties is None:
             discount = None
         else:
             discount = properties.get("discount")
             discount = None if discount == "none" else discount
 
-        brand = prod["brand"] if "brand" in keys else None
-        category = prod["category"] if "category" in keys else None
-        sub_category = prod["sub_category"] if "sub_category" in keys else None
-        sub_sub_category = prod["sub_sub_category"] if "sub_sub_category" in keys else None
-        recommendable = prod["recommendable"] if "recommendable" in keys else None
+        brand = prof["brand"] if "brand" in keys else None
+        category = prof["category"] if "category" in keys else None
+        sub_category = prof["sub_category"] if "sub_category" in keys else None
+        sub_sub_category = prof["sub_sub_category"] if "sub_sub_category" in keys else None
+        recommendable = prof["recommendable"] if "recommendable" in keys else None
         category = category[0] if type(category) == list else category
 
         # Init data
         query_data = (
-            prod["_id"],
+            prof["_id"],
             brand,
             category,
             sub_category,
@@ -49,14 +49,14 @@ if __name__ == '__main__':
 
     # Get products data
     db = client["huwebshop"]
-    col = db["products"]
-    all_products = col.find()
+    col = db["profiles"]
+    all_profiles = col.find()
 
     # Time the function
     start = time.time()
 
     with (connect_to_db() as conn, conn.cursor() as cursor):
-        insert_prod_data(all_products, cursor)
+        insert_prof_data(all_profiles, cursor)
     conn.commit()
 
     end = time.time()
