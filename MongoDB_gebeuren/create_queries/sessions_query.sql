89509b09b22b0576a3952a62806a6b394849a0b8
Ties <ties.smid@student.hu.nl>
2024-04-08 15:27:28 +0200

    sessions is nu alleen maar de profile_id en de buid zodat we met sessions_id kunnen joinen

--- MongoDB_gebeuren/create_queries/sessions_query.sql
+++ MongoDB_gebeuren/create_queries/sessions_query.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS sessions (
-    buid VARCHAR(255) PRIMARY KEY,
+    buid VARCHAR(255) NOT NULL,
     profile_id VARCHAR(255) NOT NULL,
     FOREIGN KEY(profile_id) REFERENCES profiles(id)
 )
