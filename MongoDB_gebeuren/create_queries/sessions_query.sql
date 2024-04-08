6eb7f9eb563bd472c8ac23ea938148198b7dd0ad
Ties <ties.smid@student.hu.nl>
2024-04-08 12:12:55 +0200

    sessions moest blijkbaar een primary key hebben 🤷‍♂️

--- MongoDB_gebeuren/create_queries/sessions_query.sql
+++ MongoDB_gebeuren/create_queries/sessions_query.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS sessions (
-    buid VARCHAR(255) NOT NULL,
+    buid VARCHAR(255) PRIMARY KEY,
     profile_id VARCHAR(255) NOT NULL,
     FOREIGN KEY(profile_id) REFERENCES profiles(id)
 )
