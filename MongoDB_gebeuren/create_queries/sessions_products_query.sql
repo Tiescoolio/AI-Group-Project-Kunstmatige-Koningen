d98b9c20a4a9dedc26c60d534d92790281adb5e7
Ties <ties.smid@student.hu.nl>
2024-04-08 15:27:02 +0200

    producten uit de sessie_id kunnen krijgen

--- MongoDB_gebeuren/create_queries/sessions_products_query.sql
+++ MongoDB_gebeuren/create_queries/sessions_products_query.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS sessions_products (
-    session_buid VARCHAR(255) NOT NULL,
-    FOREIGN KEY(session_buid) REFERENCES sessions(buid),
+    session_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY(session_id) REFERENCES session_id(id),
     id VARCHAR(255) NOT NULL
 )
