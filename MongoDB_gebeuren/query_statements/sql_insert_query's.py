b414a01a80edde3dc4811c9dc93fc5a343a7ba12
Ties <ties.smid@student.hu.nl>
2024-04-08 13:38:36 +0200

    execute data verkort, zodat het niet 2 keer door 2 millioen data heen loopt

--- MongoDB_gebeuren/query_statements/sql_insert_query's.py
+++ MongoDB_gebeuren/query_statements/sql_insert_query's.py
@@ -33,63 +33,56 @@ def products():
     for product in producten:
         cur.execute(products_insert_query, product)
 
+
 def profiles(profiles):
     profiles_insert_query = """INSERT INTO profiles (id) VALUES (%s)"""
     for profiel in profiles:
         profiel_id = str(profiel[0])
         cur.execute(profiles_insert_query, (profiel_id,))
 
+
 def sessions(profiles):
     ordered_insert_query = """INSERT INTO sessions (buid, profile_id) VALUES (%s, %s)"""
-    sessions = []
     for profiel in profiles:
         if profiel[1] == None:
             continue
         else:
             profiel_id = str(profiel[0])
             for buid in profiel[1]:
-                sessions.append([buid, profiel_id])
-            for session in sessions:
-                cur.execute(ordered_insert_query, session)
+                cur.execute(ordered_insert_query, [buid, profiel_id])
+
 
 def similar(profiles):
     similar_insert_query = """INSERT INTO similars (id, profile_id) VALUES (%s, %s)"""
-    vergelijkbaren = []
     for profiel in profiles:
         if profiel[2] == None:
             continue
         else:
             profiel_id = str(profiel[0])
             for vergelijkbare_id in profiel[2]:
-                vergelijkbaren.append([vergelijkbare_id, profiel_id])
-            for vergelijkbare in vergelijkbaren:
-                cur.execute(similar_insert_query, vergelijkbare)
+                cur.execute(similar_insert_query, [vergelijkbare_id, profiel_id])
+
 
 def viewed_before(profiles):
     viewed_before_insert_query = """INSERT INTO viewed_before (id, profile_id) VALUES (%s, %s)"""
-    viewed_before = []
     for profiel in profiles:
         if profiel[3] == None:
             continue
         else:
             profiel_id = str(profiel[0])
             for viewed_id in profiel[3]:
-                viewed_before.append([viewed_id, profiel_id])
-            for bekeken in viewed_before:
-                cur.execute(viewed_before_insert_query, bekeken)
+                cur.execute(viewed_before_insert_query, [viewed_id, profiel_id])
 
 
 def session_products(sessies):
     sessions_products_insert = """INSERT INTO sessions_products (sessions_buid, id) VALUES (%s, %s)"""
-    sessions = []
     for sessie in sessies:
         if sessie[1] == None:
             continue
         else:
             for product in sessie[1]:
-                sessions.append([sessie[0], product])
-            for session in sessions:
-                cur.execute(sessions_products_insert, session)
+                cur.execute(sessions_products_insert, [sessie[0], product])
+
 
 viewed_before(profielen)
 print("geexecute viewed_before")
