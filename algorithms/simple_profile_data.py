0ade0056b71b0a31e1317071100c059dc89edc25
StanMerlijn <144694971+StanMerlijn@users.noreply.github.com>
2024-04-10 01:43:13 +0200

    simpele test data profiles wordt gefilterd

--- algorithms/simple_profile_data.py
+++ algorithms/simple_profile_data.py
@@ -1,48 +1,54 @@
+import pprint
+
 from algorithms.utils import connect_to_db
 import pymongo
 import time
 
 
-def insert_prof_data(data, cur) -> None:
+def insert_prof_data(data, cur, conn) -> None:
     # Truncate existing data to avoid duplicate data
-    cur.execute("""TRUNCATE TABLE public.profiles""")
+    # cur.execute("""TRUNCATE TABLE public.profiles""")
 
-    sql_query = """
-    INSERT INTO products (id, brand, category, sub_category, sub_sub_category, aanbiedingen, recommendable)
-    VALUES (%s, %s, %s, %s, %s, %s, %s);
+    sql_query_profile = """
+    INSERT INTO profiles (id)
+    VALUES (%s);
     """
+
+
+    count = 0
     for num, prof in enumerate(data):
         keys = prof.keys()
+        if "recommendations" and "order" not in keys:
+            continue
+        count += 1
 
+        print(count, keys)
         # Set discount to None to insert correct NULL value
         # Checks for some weird products.
-        properties = prof.get("properties", {})
-        if properties is None:
-            discount = None
-        else:
-            discount = properties.get("discount")
-            discount = None if discount == "none" else discount
+        prof_id = str(prof["_id"])
+        # cur.execute("""INSERT INTO profiles (id) VALUES (%s)""", prof_id)
 
-        brand = prof["brand"] if "brand" in keys else None
-        category = prof["category"] if "category" in keys else None
-        sub_category = prof["sub_category"] if "sub_category" in keys else None
-        sub_sub_category = prof["sub_sub_category"] if "sub_sub_category" in keys else None
-        recommendable = prof["recommendable"] if "recommendable" in keys else None
-        category = category[0] if type(category) == list else category
+        order = prof.get("order", {})
+        if order:
+            ids = order.get("ids")
+            # print(ids, order)
+
+        prev_rec = prof["previously_recommended"] if "previously_recommended" in keys else None
+
+        # category = prof["category"] if "category" in keys else None
+        # sub_category = prof["sub_category"] if "sub_category" in keys else None
+        # sub_sub_category = prof["sub_sub_category"] if "sub_sub_category" in keys else None
+        # recommendable = prof["recommendable"] if "recommendable" in keys else None
+        # category = category[0] if type(category) == list else category
 
         # Init data
-        query_data = (
-            prof["_id"],
-            brand,
-            category,
-            sub_category,
-            sub_sub_category,
-            discount,
-            recommendable
-        )
-        print(num, query_data)
+
+        # print(num, query_data)
         # print(f"{category}, {sub_category}, {sub_sub_category}")
-        cur.execute(sql_query, query_data)
+        # cur.execute(sql_query, query_data)
+        if count % 10**6 == 0:
+            conn.commit()
+
 
 if __name__ == '__main__':
     client = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -56,8 +62,11 @@ if __name__ == '__main__':
     start = time.time()
 
     with (connect_to_db() as conn, conn.cursor() as cursor):
-        insert_prof_data(all_profiles, cursor)
-    conn.commit()
+        insert_prof_data(all_profiles, cursor, conn)
+    # conn.commit()
 
     end = time.time()
     print(f"data transfer time = {end - start:.4f}s")  # Last time is 6.5784s, 6.8136s
+    # Time for all looping all elements = 34.5722s, amount of profiles = 2_081_649
+    # Time for skipping useles data = 33.1787s, amount of profiles = 2_017_935
+    # Time for skipping all useles data = 17.6500s, amount of profiles = 102_712
