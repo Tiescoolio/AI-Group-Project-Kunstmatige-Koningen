d1e8bd66307043b65a3ad4cb897916bdb368ff5b
Tomkr <tomkruijk@gmail.com>
2024-04-10 12:05:00 +0200

    Oke, ik denk dat dit nu écht het laatste is van de dataoverzet, er zijn veranderingen geweest omtrent de ERD, maar die zijn nu van de "mongoDB kant naar python" kant af

--- MongoDB_gebeuren/query_statements/mongodb_data/sessions_products.py
+++ MongoDB_gebeuren/query_statements/mongodb_data/sessions_products.py
@@ -1,28 +1,39 @@
-#we moeten de _id, has_sale en order.products(kunnen er meerdere of geen zijn) hebben
-# BUID & _id (Waar er een sale is)
+#_ID & ProductID
+
+
+
+
+
+#RETURN = BUID & PRODUCTEN BIJ DIE BUID
+#LIJST met [[BUID] - [ALLE producten]]
+# Van alles dat has sale heeft
+# ALs order null is weglaten anders lijst maken met alle producten die erin staan
+# Producten aan buid linken
 
 from IPython.display import display
 from pymongo import MongoClient
 import pandas
+
 pandas.set_option('display.max_rows', None)
 pandas.set_option('display.max_columns', None)
 pandas.set_option('display.width', None)
 
 def connect_to_mongo(host, port, db):
-    conn = MongoClient(host,port)
+    conn = MongoClient(host, port)
     return conn[db]
 
 def turn_mongo_to_sql():
     data = get_mongo()
     display(data)
 
+import time
 def get_mongo():
     collectie = "sessions"
     database = connect_to_mongo("localhost", 27017, "huwebshop")
-    cursor = database[collectie].find({}, {"_id":1, "has_sale":1, "buid": 1})
+    cursor = database[collectie].find({}, {"buid":1, "order.products":1})
+
     data = pandas.DataFrame(list(cursor))
     data = data.where(pandas.notnull(data), None)
-
     templist = []
     for index, row in data.iterrows():
         indices = []
@@ -34,13 +45,20 @@ def get_mongo():
                 indices.append(i)
         templist.append(indices)
 
-    sessions = []
+    buid_list = []
     for i in templist:
         temp = []
-        if i[2] == True:
-            temp.append(i[0])
+        if i[2] is not None:
             temp.append(i[1])
-            sessions.append(temp)
-    return sessions
+            temptest = []
+            for j in i[2]:
+                if isinstance(j, dict):
+                    for k in j.values():
+                        temptest.append(k)
+                else:
+                    temptest.append(j)
+            temp.append(temptest)
+            buid_list.append(temp)
+    return buid_list
 
 turn_mongo_to_sql()
