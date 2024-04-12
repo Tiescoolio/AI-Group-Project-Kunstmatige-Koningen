e47edfb8d09eeb6fbe31bf78fb32dc621c98bbb8
Ties <ties.smid@student.hu.nl>
2024-04-12 11:55:08 +0200

    variabele naam verandering

--- algorithms/similar_costumer_algorithm/products_from_profile.py
+++ algorithms/similar_costumer_algorithm/products_from_profile.py
@@ -2,14 +2,14 @@ from algorithms.utils import connect_to_db as connect
 con = connect()
 cur = con.cursor()
 
-def producten_van_profiel(profiel_id):
-    products_bij_profiel_id = f"""
+def products_from_profile(profile_id):
+    products_from_profile_query = f"""
         SELECT id FROM sessions_products
         INNER JOIN sessions ON sessions_buid = buid
-        WHERE profile_id = '{profiel_id}';
+        WHERE profile_id = '{profile_id}';
     """
-    cur.execute(products_bij_profiel_id)
-    lijst = [id[0] for id in cur.fetchall()]
-    return lijst
+    cur.execute(products_from_profile_query)
+    list = [id[0] for id in cur.fetchall()]
+    return list
 if __name__ == '__main__':
-    print(producten_van_profiel('5a393d68ed295900010384ca'))
+    print(products_from_profile('5a393d68ed295900010384ca'))
